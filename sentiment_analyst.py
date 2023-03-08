cb38655b45065a91e9e413788a4c093ef46af0ca
Tegar Adisaputra <tegara25@gmail.com>
2023-03-08 19:12:03 +0700

    perbaikan

--- sentiment_analyst.py
+++ sentiment_analyst.py
@@ -34,13 +34,11 @@ def analyst():
 
 
 
-@app.route('/twitter_scrap', methods= ['POST'])
+@app.route('/twitter_scrap', methods=['POST'])
 def sentiment_analysis():
     query = request.form.get('keyword')
-
-    # query = "(#jakarta)"
     tweets = []
-    limit = request.form.get('limit')
+    limit = int(request.form.get('limit'))
 
     for tweet in sntwitter.TwitterSearchScraper(query).get_items():
         if len(tweets) == limit:
@@ -56,18 +54,18 @@ def sentiment_analysis():
         tweet_prop["Date"] = row['Date']
         tweet_prop["User"] = row['User']
         tweet_prop["Tweet"] = row['Tweet']
-        bersih = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",str(row['Tweet'])).split())
-        analyst = tb(bersih)
+        clean_tweet = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",str(row['Tweet'])).split())
+        analyst = tb(clean_tweet)
         if analyst.sentiment.polarity > 0.0:
             tweet_prop["Sentiment"] = "positive"
         elif analyst.sentiment.polarity == 0.0:
             tweet_prop["Sentiment"] = "neutral"
-        else :
+        else:
             tweet_prop["Sentiment"] = "negative"
         sentiments.append(tweet_prop)
 
     result = pd.DataFrame(sentiments)
-    data = result.to_json(orient= 'index')
+    data = result.to_json(orient='index')
     positive = int((result["Sentiment"] == "positive").sum())
     negative = int((result["Sentiment"] == "negative").sum())
     neutral = int((result["Sentiment"] == "neutral").sum())
@@ -77,7 +75,8 @@ def sentiment_analysis():
 
     sentiment_distribution = dict(zip(labels, counts))
 
-    return jsonify(sentiment_distribution, data)
+    return jsonify(sentiment_distribution=sentiment_distribution, data=data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
